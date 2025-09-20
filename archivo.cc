5359e47fc317b45d533914b1894b304f97d2e4f7
AnderTurcios <00003925@uca.edu.sv>
2025-09-19 19:43:02 -0600

    feature: Agregar funcion para pedir nombre

--- archivo.cc
+++ archivo.cc
@@ -2,7 +2,10 @@
 
 int main()
 {
+    std::string nombre;
     std::cout << "hola mundo\n";
-    
+    std::cout << "Ingresa tu nombre: ";
+    std::cin >> nombre;
+    std::cout << "Hola " << nombre;
     return 0;
 }
